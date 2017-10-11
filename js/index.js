1d2d499df89e90d049c3b0cbcf5664e7e5319a67
Flatiron School <flatironschool@Flatirons-Air-3.home>
2017-10-10 22:14:46 -0400

    style changes. Added fades to section content. Made nav links work.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
 var options = {
-  strings: ["I'm Web Developer", "I'm Web Designer", "I'm Software Engineer", "I'm Tyler Knight"],
+  strings: ["I'm a Web Developer", "I'm a Web Designer", "I'm a Software Engineer", "I'm Tyler Knight"],
   onComplete: function(self) { displaySub() },
   typeSpeed: 60
 }
@@ -15,12 +15,31 @@ const displaySub = () => {
 
 
 $('#intro').height($(window).height())
+$('.bgc').height($(window).height())
 
 $(window).resize(function() {
     $('#intro').height($(window).height());
+    $('.bgc').height($(window).height());
 });
 
 $(window).trigger('resize');
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 
 
 // Scroll events
