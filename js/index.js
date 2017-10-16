b3d1a2732dc74c1752609b2e01ad8ba283f68cd0
Flatiron School <flatironschool@Flatirons-Air-3.home>
2017-10-16 13:26:55 -0400

    post launch tweaks

--- js/index.js
+++ js/index.js
@@ -59,6 +59,27 @@ $(function() {
 });
 
 
+//helper function
+function $$(selector, context) {
+    context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+}
+
+window.addEventListener("scroll", function() {
+    var scrolledHeight= window.pageYOffset;
+    $$(".parallax").forEach(function(el,index,array) {
+        var limit= el.offsetTop+ el.offsetHeight;
+        if(scrolledHeight > el.offsetTop && scrolledHeight <= limit) {
+            el.style.backgroundPositionY= (scrolledHeight - el.offsetTop) /1.5+ "px";
+        }
+        else {
+            el.style.backgroundPositionY= "0";
+        }
+    });
+});
+
+
 
 
 
